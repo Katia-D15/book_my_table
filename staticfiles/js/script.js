f171f4af06299cf2b133f28940d66e6082dc399c
Katia-D15 <0j0zirbo@students.codeinstitute.net>
2025-05-15 12:33:57 +0100

    chore: configure collectstatic for Heroku deployment

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,1 +1,17 @@
-console.log("Welcome to BookMyTable!");
+/**
+ * Displays the edit form for a specific booking.
+ */
+
+function showEditForm(bookingId) {
+    const confirmed = confirm("Are you sure you want to edit this booking?");
+    if (!confirmed) return;
+
+    const allForms = document.querySelectorAll("[id^=edit-form-]");
+    allForms.forEach(form => form.style.display="none");
+
+    const formToShow = document.getElementById(`edit-form-${bookingId}`)
+    if (formToShow) {
+        formToShow.classList.remove("d-none");
+        formToShow.style.display = "block";
+    }
+}
