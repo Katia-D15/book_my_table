84124bd6631d90d71e9a5d8abdf394f067d3e87b
Katia-D15 <0j0zirbo@students.codeinstitute.net>
2025-05-18 17:57:12 +0100

    feat: auto-dismiss alerts | chore: code cleanup

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,5 +1,11 @@
+const alerts = document.querySelectorAll('.alert');
+
 /**
  * Displays the edit form for a specific booking.
+ * 
+ * Requests user confirmation before proceeding.
+ * Ensures that only the form corresponding to the booking is displayed,
+ * hiding all other editing forms.
  */
 
 function showEditForm(bookingId) {
@@ -9,9 +15,23 @@ function showEditForm(bookingId) {
     const allForms = document.querySelectorAll("[id^=edit-form-]");
     allForms.forEach(form => form.style.display="none");
 
-    const formToShow = document.getElementById(`edit-form-${bookingId}`)
+    const formToShow = document.getElementById(`edit-form-${bookingId}`);
     if (formToShow) {
         formToShow.classList.remove("d-none");
         formToShow.style.display = "block";
     }
 }
+
+/**
+ * Automatically hides success and info alert messages after 3.5 seconds.
+ * 
+ * Applies to elements with the 'alert-success' or 'alert-info' classes.
+ */
+alerts.forEach((alert) => {
+    if (alert.classList.contains('alert-success') || alert.classList.contains('alert-info')) {
+        setTimeout(() => {
+            const alertInstance = new bootstrap.Alert(alert);
+            alertInstance.close();
+        }, 3500);
+    }
+});
